6990c528a4ceae398aaf99bdc94aef0e89b1b4e1
sjm-study <50009276+sjm-study@users.noreply.github.com>
2021-06-08 11:08:08 +0800

    Revert "tt"
    This reverts commit e0452cf5f295b0a12b7e573a02ee1588ea52a008.

--- hotManagerProject/src/index.js
+++ hotManagerProject/src/index.js
@@ -61,7 +61,7 @@ const Index = () => {
             PoPular
           </a>
           <a
-            hre2f="#/battle"
+            h ref="#/battle"
             className={route.page === "battle" ? styles.active : ""}
           // onClick={(e)=>{console.log(history)}}
           // onKeyDown={()=>window.location.href='#/battle'}
