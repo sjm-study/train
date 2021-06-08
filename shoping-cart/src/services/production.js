2f735d63cf565d87abdeacf0d0ec0e9c892a6159
sjm-study <50009276+sjm-study@users.noreply.github.com>
2021-06-08 11:28:15 +0800

    1

--- shoping-cart/src/services/production.js
+++ shoping-cart/src/services/production.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default function getData() {
     // return axios.get('./data/products.json')
-    return axios.get('./products.json')
+    return axios.get('products.json')
 }
