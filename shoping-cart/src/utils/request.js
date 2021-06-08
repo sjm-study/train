f007e8dfb96da985881b3fd961380e48484ecad2
sjm-study <50009276+sjm-study@users.noreply.github.com>
2021-06-08 11:04:44 +0800

    88

--- shoping-cart/src/utils/request.js
+++ shoping-cart/src/utils/request.js
@@ -21,14 +21,10 @@ function checkStatus(response) {
  * @param  {object} [options] The options we want to pass to "fetch"
  * @return {object}           An object containing either "data" or "err"
  */
-// export default function request(url, options) {
-//   return fetch(url, options)
-//     .then(checkStatus)
-//     .then(parseJSON)
-//     .then(data => ({ data }))
-//     .catch(err => ({ err }));
-// }
-
 export default function request(url, options) {
-    return fetch(url,{...options})
+  return fetch(url, options)
+    .then(checkStatus)
+    .then(parseJSON)
+    .then(data => ({ data }))
+    .catch(err => ({ err }));
 }
